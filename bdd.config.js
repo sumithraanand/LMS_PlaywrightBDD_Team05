2aaa0b22c881917f1ace9935188bd93bd0a13393
anandsravi <anandsravi@gmail.com>
2026-08-11 11:23:23 -0400

    Synchronize Sravanthi_LMS with Sumithra_LMS

--- bdd.config.js
+++
@@ -1,7 +0,0 @@
-import { defineBddConfig } from 'playwright-bdd';
-
-export const testDir = defineBddConfig({
-  features: 'features/**/*.feature',
-  steps: 'stepDefinitions/**/*.js',
-  outputDir: '.features-gen'
-});
